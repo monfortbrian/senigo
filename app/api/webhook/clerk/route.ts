6de7f3aebb21741692b00c428d99e3aadb38530e
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2024-04-16 09:09:38 +0200

    Update route.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,6 +56,7 @@ export async function POST(req: Request) {
  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    console.log("event data:",evt.data);
 
     const user = {
       clerkId: id,
