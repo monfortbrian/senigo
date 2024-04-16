f8841b26fc5e684ab3dd03d9ca41cf04074745a5
Trezzmann <anahimana3@gmail.com>
2024-04-16 15:55:55 +0200

    some changes

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,28 +56,27 @@ export async function POST(req: Request) {
  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-   throw new Error(JSON.stringify(evt.data))
-   return NextResponse.json({ message: 'OK', user: JSON.stringify(evt.data) })
-    // const user = {
-    //   clerkId: id,
-    //   email: email_addresses[0].email_address,
-    //   username: "Trezzmann",
-    //   firstName: first_name,
-    //   lastName: last_name,
-    //   photo: image_url,
-    // }
+   
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    }
 
-    // const newUser = await createUser(user);
+    const newUser = await createUser(user);
 
-    // if(newUser) {
-    //   await clerkClient.users.updateUserMetadata(id, { 
-    //     publicMetadata: {
-    //       userId: newUser._id
-    //     }
-    //   })
-    // }
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, { 
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
 
-    // return NextResponse.json({ message: 'OK', user: newUser })
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
   if (eventType === 'user.updated') {
@@ -86,7 +85,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name,
       lastName: last_name,
-      username: username!,
+      username: username,
       photo: image_url,
     }
 
